f46c9c907fa1869d72e69f11bbcb105e8768e865
KaisarH007 <kaisarh007@gamil.com>
2021-10-04 01:09:16 +0600

    first commit

---
+++ src/fakeData.js
@@ -0,0 +1,66 @@
+const data = [
+  {
+    titel: "Data Science",
+    price: 200,
+    img: "https://assets.kpmg/is/image/kpmg/blue-colored-data-science-illustration?scl=1",
+    description:
+      "Develop skills for tackling business challenges with a data-driven mindset. Apply today.",
+    id: 1,
+  },
+  {
+    titel: "Web Development",
+    price: 250,
+    img: "https://www.onlinecoursereport.com/wp-content/uploads/2020/07/shutterstock_394793860-1024x784.jpg",
+    description:
+      "Learn modern Web Development fundamentals as well as advanced topics. Start today.",
+    id: 2,
+  },
+  {
+    titel: "Music",
+    price: 150,
+    img: "https://www.ed2go.com/common/images/1/17216.jpg",
+    description:
+      "Learn music online. Best online guitar, piano, ukulele, autoharp and DJ courses online.",
+    id: 3,
+  },
+  {
+    titel: "Photography",
+    price: 300,
+    img: "https://images.shiksha.com/mediadata/images/articles/1568193656phpmWI2xq.jpeg",
+    description:
+      "Learn how to take amazing photos in this online photography course for all levels.",
+    id: 4,
+  },
+  {
+    titel: "IT & Software",
+    price: 180,
+    img: "https://d1vwxdpzbgdqj.cloudfront.net/assets/domain-specific-page/se-banner-03d237fdaf648377083bb9ffe79ac4dda695d820e15f35afed0a60f319bcd7ba.png",
+    description:
+      "Best Software Courses for Freshers in 2021 discussed in length.",
+    id: 5,
+  },
+  {
+    titel: "Marketing",
+    price: 150,
+    img: "https://conseltex.com/wp-content/uploads/2020/07/Digiskillz-Online-01-min.png",
+    description:
+      "Stay ahead. Be up to speed on the latest insights and trends in APAC. Ideas. Inspiration..",
+    id: 6,
+  },
+  {
+    titel: "Graphics Design",
+    price: 100,
+    img: "https://cdn.dribbble.com/users/1731254/screenshots/11503568/media/a76ce92b7459134f0aaee5b60f518df1.png",
+    description:
+      "Learn Photoshop, Illustrator, and InDesign by creating posters, logos, and more.",
+    id: 7,
+  },
+  {
+    titel: "Self Development",
+    price: 120,
+    img: "http://risewithsujan.com/wp-content/uploads/2020/01/SELF-2.jpg",
+    description:
+      "You will learn how to be more confident, happier, productive and sucessful.",
+    id: 8,
+  },
+];
